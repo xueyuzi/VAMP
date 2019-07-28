8a48e31d24845ae066377af558fb27f70f9901c3
鳕鱼子 <xueyu.d@gmail.com>
2019-07-28 19:52:02 +0800

    fixed #34

--- src/app/custom/system-manage/role/role.component.ts
+++ src/app/custom/system-manage/role/role.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RoleService } from './role.service';
+import { TreeNode, ConfirmationService } from 'primeng/api';
+import { MenuService } from '../menu/menu.service';
 
 @Component({
   selector: 'ngx-role',
@@ -33,31 +35,87 @@ export class RoleComponent implements OnInit {
     actions: {
       add: false,
       edit: false,
-      delete: false,
-
+    },
+    delete: {
+      confirmDelete: true,
     }
   }
-  roleList: Observable<Array<any>>
+  roleList: Array<any>;
   role: any;
-  type:string;
   isEdit: boolean = false;
-  constructor(private roleService: RoleService) { }
+  menus: TreeNode[];
+
+  constructor(private roleService: RoleService,
+    private menuService: MenuService,
+    private confirmationService: ConfirmationService) { }
 
   ngOnInit() {
-    this.role={}
-    this.roleList = this.roleService.getList();
+    this.role = {};
+    this.getList();
+
   }
   showNew() {
-    this.type = "add";
-    this.role = {};
-    this.isEdit = true;
+    this.role = {
+      roleSort: 1,
+      status: 0,
+    };
+    this.menuService.getMenusWithTreeData().subscribe(
+      menus => {
+        this.menus = menus;
+        this.isEdit = true;
+      }
+
+    );
+  }
+  delRole($event) {
+    this.confirmationService.confirm({
+      message: '你确定要删除此用户么？',
+      accept: () => {
+        this.roleService.del($event.data.roleId).subscribe(
+          res => {
+            this.getList();
+          }
+        )
+      }
+    });
   }
   showEdit($event) {
-    this.type = "edit";
     this.role = $event.data;
-    this.isEdit = true;
+    this.menuService.getMenusWithTreeData(this.role.roleId).subscribe(
+      menus => {
+        this.menus = menus;
+        this.isEdit = true;
+      }
+    );
   }
-  saveRole(){
+
+
+  changeMenus(event, node) {
+    let i = this.menus.indexOf(node);
+    this.menus[i].children.forEach(
+      (v, k) => {
+        this.menus[i].children[k].data.checked = event;
+      }
+    )
+  }
+  getList() {
+    this.roleService.getList().subscribe(
+      roles => this.roleList = roles
+    );
+  }
+  saveRole() {
+    let menuIds = [];
+    this.menus.forEach(menu1 => {
+      menu1.data.checked && menuIds.push(menu1.data.id);
+      menu1.children.forEach(
+        menu2 => menu2.data.checked && menuIds.push(menu2.data.id)
+      )
+    })
+    this.role.menuIds = menuIds.join(',');
+    this.roleService.save(this.role).subscribe(res => {
+      this.getList();
+      this.isEdit = false;
+    });
 
   }
 
