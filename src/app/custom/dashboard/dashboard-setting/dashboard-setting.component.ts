5d95a316397b779a87b6e8e41f1394c8b0eb89f8
鳕鱼子 <xueyu.d@gmail.com>
2019-08-04 12:27:49 +0800

    fixed #69

--- src/app/custom/dashboard/dashboard-setting/dashboard-setting.component.ts
+++ src/app/custom/dashboard/dashboard-setting/dashboard-setting.component.ts
@@ -1,3 +1,4 @@
+import { JsonEditorService } from './../../../common/json-editor.service';
 import { Component, OnInit } from '@angular/core';
 import { DashboardSettingService } from './dashboard-setting.service';
 import { Observable } from 'rxjs';
@@ -12,7 +13,8 @@ export class DashboardSettingComponent implements OnInit {
 
   constructor(
     private dashboardSettingService: DashboardSettingService,
-    private toastrService: NbToastrService
+    private toastrService: NbToastrService,
+    private jsonEditorService:JsonEditorService
   ) { }
   isShow: boolean;
   chartSetting: any;
@@ -22,26 +24,36 @@ export class DashboardSettingComponent implements OnInit {
     this.dashboardSettingService.show$.subscribe(
       flag => this.isShow = flag
     )
+  }
 
+  setEditor() {
+    setTimeout(() => {
+      this.jsonEditorService.createEditor("chart-setting-editor");
+      this.jsonEditorService.setValue(this.chartSetting.es_json)
+    }, 50)
   }
   onShow() {
+    this.tag = "data"
 
     this.dashboardSettingService.getData().subscribe(
       data => {
         this.container = this.dashboardSettingService.container
-        this.chartSetting = Object.assign(this.container.panelData.chartStyle,data);
+        this.chartSetting = Object.assign(this.container.panelData.chartStyle, data);
       }
     )
   }
   onHide() {
     this.dashboardSettingService.toggle(false);
+    
   }
   save() {
+    this.chartSetting.es_json = this.jsonEditorService.getValue();
     this.dashboardSettingService.save(this.chartSetting).subscribe(res => {
       this.dashboardSettingService.toggle(false);
     });
   }
   changeAction(action) {
+    this.setEditor();
     this.tag = action;
   }
 }
